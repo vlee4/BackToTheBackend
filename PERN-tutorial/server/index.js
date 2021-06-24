f23d96f581c71112c6fd73c59d4aabc47a8d6e41
Victoria <vicglee1234007@gmail.com>
2021-06-24 17:27:05 -0500

    Set up express

---
+++ PERN-tutorial/server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+//Middleware
+app.use(cors()); //Allows frontend & backend to communicate despite being on diff ports
+app.use(express.json()); //Allows server to get data from client-side via req body
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
