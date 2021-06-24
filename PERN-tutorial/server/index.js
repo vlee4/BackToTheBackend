14e72a5c238105b3fffa36ecc755be616fa8b034
Victoria <vicglee1234007@gmail.com>
2021-06-24 18:40:35 -0500

    Set up postgres db, in progress

--- PERN-tutorial/server/index.js
+++ PERN-tutorial/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const pool = require("./db");
 
 //Middleware
 app.use(cors()); //Allows frontend & backend to communicate despite being on diff ports
