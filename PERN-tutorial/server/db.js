1a549196a267851afc28232fd98462ec8fed1e68
Victoria <vicglee1234007@gmail.com>
2021-06-24 22:22:02 -0500

    Create pg pool

--- PERN-tutorial/server/db.js
+++ PERN-tutorial/server/db.js
@@ -1,14 +1,14 @@
 const { Pool } = require("pg");
 //Pooling allows postgresql to  have a resuable group of clients that you can checkout, use & return instead of slowing down your app with a limited queue of different clients (which would slow down the app)
 
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   port: 5432,
-//   database: "perntodo",
-// });
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "perntodo",
+});
 
-const pool = new Pool();
+// const pool = new Pool();
 
 module.exports = pool;
 
