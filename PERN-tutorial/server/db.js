bd1560873ef98ac21bc25fc15400d373373efb7b
Victoria <vicglee1234007@gmail.com>
2021-06-24 22:57:47 -0500

    Experiment with db reading env variables

--- PERN-tutorial/server/db.js
+++ PERN-tutorial/server/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 //Pooling allows postgresql to  have a resuable group of clients that you can checkout, use & return instead of slowing down your app with a limited queue of different clients (which would slow down the app)
 
+console.log("user", process.env.USER, "env", process.env);
+
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "perntodo",
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
 });
 
 // const pool = new Pool();
