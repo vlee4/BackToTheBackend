f73fa38ec897a4b06112f6baf867b1d6bdc7276a
Victoria <vicglee1234007@gmail.com>
2021-07-06 23:50:48 -0500

    Add onSubmit method to record new todos in db

--- PERN-tutorial/client/src/Components/InputTodo.js
+++ PERN-tutorial/client/src/Components/InputTodo.js
@@ -3,10 +3,25 @@ import React, { Fragment, useState } from "react";
 const InputTodo = () => {
   const [description, setDescription] = useState("");
 
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">INPUT TODO</h1>
-      <form className="d-flex mt-5">
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
           className="form-control"
