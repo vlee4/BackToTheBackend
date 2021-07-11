e6e8c6c91a7c73163d534ecdb68e8ad3d2b2fe38
vlee4 <vicglee1234007@gmail>
2021-07-10 21:39:11 -0500

    Rename files for easier access

--- Node-Express/app.js
+++ Node-Express/app.js
@@ -1,9 +1,9 @@
 //CommonJS: every file is a module (by default)
 //ES6 Modules: Encapsulated Code (only share minimum)
-const {pog, jill} = require('./4 - names')
-const sayHi = require('./5 - utils')
-const data = require("./6 - alt")
-require('./7 - anotherAlt');
+const {pog, jill} = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require("./6-alt")
+require('./7-anotherAlt');
 //When you import a module you invoke the code from that file
 
 sayHi('susan')
