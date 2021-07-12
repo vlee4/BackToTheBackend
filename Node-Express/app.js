a15e35635a2c79cb3d297fa77987ccdc98fe60bf
Victoria <vicglee1234007@gmail.com>
2021-07-12 17:50:17 -0500

    Move module-related code to separate file for organization

--- Node-Express/app.js
+++ Node-Express/app.js
@@ -1,11 +1,4 @@
-//CommonJS: every file is a module (by default)
-//ES6 Modules: Encapsulated Code (only share minimum)
-const {pog, jill} = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require("./6-alt")
-require('./7-anotherAlt');
-//When you import a module you invoke the code from that file
+//Built-in Modules
+//os, path, fs, http
 
-sayHi('susan')
-sayHi(pog)
-sayHi(jill)
+//OS Module
